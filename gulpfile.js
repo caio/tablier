9114f372b99b7dddccc0ee63bf2d9ff4e5ccc617
Caio Romão <hello@caio.co>
2019-02-01 11:42:23 +0100

    Stop generating html via gulp+mustache
    Now gulp is only used to build/minify/clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var browserSync = require('browser-sync');
 var gulp = require('gulp');
 var gzip = require('gulp-gzip');
 var postcss = require('gulp-postcss')
@@ -8,20 +7,10 @@ var uncss = require('postcss-uncss')
 var cssnano = require('cssnano')
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
-var mustache = require('gulp-mustache');
 var fs = require('fs');
 
 var pkg = require('./package.json');
 var dirs = pkg['configuration'].directories;
-var reload = browserSync.reload;
-
-var browserSyncOptions = {
-    logPrefix: 'TABLIER',
-    notify: false,
-    open: false,
-    port: 1313,
-    injectChanges: false,
-};
 
 gulp.task('sass', function() {
     return gulp.src(dirs.src + '/sass/style.scss')
@@ -29,70 +18,7 @@ gulp.task('sass', function() {
         .pipe(gulp.dest(dirs.src + '/css/'));
 });
 
-gulp.task('html_basic', function() {
-    return gulp.src([
-            dirs.src + '/template/index.mustache',
-            dirs.src + '/template/error.mustache',
-            dirs.src + '/template/search.mustache',
-            dirs.src + '/template/zero_results.mustache',
-        ]).pipe(mustache(dirs.src + '/render_data.json'))
-        .pipe(rename(function(path) { path.extname = ".html"; }))
-        .pipe(gulp.dest(dirs.src));
-});
-
-function getRenderDataSync() {
-    var dataFile = './' + dirs.src + '/render_data.json';
-    var renderData = JSON.parse(fs.readFileSync(dataFile, 'utf8'));
-    return renderData;
-}
-
-function renderCustomSearch(suffix, cb) {
-    var renderData = getRenderDataSync();
-    cb(renderData);
-    return gulp.src(dirs.src + '/template/search.mustache')
-        .pipe(mustache(renderData))
-        .pipe(rename(function(path) { path.basename += suffix; path.extname = ".html"; }))
-        .pipe(gulp.dest(dirs.src));
-};
-
-gulp.task('html_index_unstable', function() {
-    var renderData = getRenderDataSync();
-    renderData.show_unstable_warning = true;
-    renderData.search_is_disabled = true;
-    return gulp.src(dirs.src + '/template/index.mustache')
-        .pipe(mustache(renderData))
-        .pipe(rename(function(path) { path.basename += "_unstable"; path.extname = ".html"; }))
-        .pipe(gulp.dest(dirs.src));
-});
-
-gulp.task('html_search_no_next', function() {
-    return renderCustomSearch('_no_next', function(data) {
-        data.pagination_next_href = null;
-    });
-});
-
-gulp.task('html_search_no_prev', function() {
-    return renderCustomSearch('_no_prev', function(data) {
-        data.pagination_prev_href = null;
-    });
-});
-
-gulp.task('html_search_no_both', function() {
-    return renderCustomSearch('_no_both', function(data) {
-        data.pagination_next_href = null;
-        data.pagination_prev_href = null;
-    });
-});
-
-gulp.task('html',
-    gulp.parallel(
-        'html_basic',
-        'html_index_unstable',
-        'html_search_no_next',
-        'html_search_no_prev',
-        'html_search_no_both'));
-
-gulp.task('css', gulp.series('html', 'sass', function () {
+gulp.task('css', gulp.series('sass', function () {
     var postConfig = [
         uncss({
             html: [
@@ -120,19 +46,4 @@ gulp.task('build', gulp.series('css', function () {
             .pipe(gulp.dest(dirs.src + '/css/'));
 }));
 
-gulp.task('serve', gulp.series('css', function () {
-
-    browserSyncOptions.server = dirs.src;
-    browserSync(browserSyncOptions);
-
-    gulp.watch([
-        dirs.src + '/template/**/*.mustache',
-        dirs.src + '/render_data.json',
-    ], gulp.series('css'));
-
-    gulp.watch([
-        dirs.src + '/sass/**/*.scss'
-    ], gulp.series('sass'));
-}));
-
-gulp.task('default', gulp.series('serve'));
+gulp.task('default', gulp.series('build'));
